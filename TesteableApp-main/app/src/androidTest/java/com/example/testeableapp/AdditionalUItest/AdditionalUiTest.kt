717923519ad1fc97baad94fb59cf2ba5ed5d6d6c
RamirezC33 <00084020@uca.edu.sv>
2025-06-18 16:42:12 -0600

    Text test final

---
+++ TesteableApp-main/app/src/androidTest/java/com/example/testeableapp/AdditionalUItest/AdditionalUiTest.kt
@@ -0,0 +1,31 @@
+package com.example.testeableapp.AdditionalUItest
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.testeableapp.ui.Screens.TipCalculatorScreen
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AdditionalUITest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun invalidInputDoesNotCrashAndShowsZeroTip() {
+        composeTestRule.setContent {
+            TipCalculatorScreen()
+        }
+
+        composeTestRule
+            .onNodeWithTag("BillAmount")
+            .performTextInput("texto")
+
+        composeTestRule
+            .onNodeWithText("Propina: $0.00")
+            .assertIsDisplayed()
+    }
+}
